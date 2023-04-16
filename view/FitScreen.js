f73e816c4d68ee00ba5a7eb4a77140cfda158ced
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-04-16 08:57:44 +0600

    code refactoring

--- view/FitScreen.js
+++ view/FitScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import React, { useState } from "react";
 import { useRoute } from "@react-navigation/native";
 const FitScreen = () => {
@@ -10,7 +10,13 @@ const FitScreen = () => {
   console.log(current, "first exercise");
   return (
     <View style={styles.firstContainer}>
-        <Image style={styles.imageStyle} source={{ uri: current.image }} />
+      <Image style={styles.imageStyle} source={{ uri: current.image }} />
+      <Text style={styles.excersiseNameStyle}>{current.name}</Text>
+      <Text style={styles.excersiseSetStyle}>x{current.sets}</Text>
+
+      <Pressable style={styles.bottomPressableStyle}>
+        <Text style={styles.bottomPressableTextStyle}>Done</Text>
+      </Pressable>
     </View>
   );
 };
@@ -19,10 +25,39 @@ export default FitScreen;
 
 const styles = StyleSheet.create({
   firstContainer: {
-    marginTop: 50,
+    marginTop: 40,
   },
   imageStyle: {
     width: "100%",
-    height: 380,
+    height: 450,
+  },
+  excersiseNameStyle: {
+    marginTop: 15,
+    color: "purple",
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  excersiseSetStyle: {
+    marginTop: 10,
+    color: "purple",
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  bottomPressableStyle: {
+    backgroundColor: "purple",
+    padding: 10,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginVertical: 20,
+    width: 180,
+    borderRadius: 10,
+  },
+  bottomPressableTextStyle: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
