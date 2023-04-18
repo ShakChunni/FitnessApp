b429cc2007743ec4113080e78e195a494cd823a5
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-04-18 17:09:47 +0600

    code refactoring

---
+++ test/test.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const test = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  )
+}
+
+export default test
+
+const styles = StyleSheet.create({})
